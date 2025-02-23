0df6a99a034b6ccbf42864ff7b19d80e1f3f3177
Ola Smaaberg <olafs@live.com>
2025-02-24 00:22:02 +0100

    Add if/else parsing

--- parser/parser/parser.go
+++ parser/parser/parser.go
@@ -61,6 +61,8 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerPrefix(token.MINUS, p.parsePrefixExpression)
 	p.registerPrefix(token.TRUE, p.parseBoolean)
 	p.registerPrefix(token.FALSE, p.parseBoolean)
+	p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
+	p.registerPrefix(token.IF, p.parseIfExpression)
 
 	p.infixParseFns = make(map[token.TokenType]infixParseFn)
 	p.registerInfix(token.PLUS, p.parseInfixExpression)
@@ -159,16 +161,67 @@ func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
 	return stmt
 }
 
+func (p *Parser) parseGroupedExpression() ast.Expression {
+	p.nextToken()
+	exp := p.parseExpression(LOWEST)
+
+	if !p.expectNextToken(token.RPAREN) {
+		return nil
+	}
+
+	return exp
+}
+
+func (p *Parser) parseIfExpression() ast.Expression {
+	e := &ast.IfExpression{Token: p.curToken}
+	if !p.expectNextToken(token.LPAREN) {
+		return nil
+	}
+	p.nextToken()
+	e.Condition = p.parseExpression(LOWEST)
+
+	if !p.expectNextToken(token.RPAREN) {
+		return nil
+	}
+	if !p.expectNextToken(token.LBRACE) {
+		return nil
+	}
+	e.Consequence = p.parseBlockStatement()
+
+	if p.peekTokenIs(token.ELSE) {
+		p.nextToken()
+		if !p.expectNextToken(token.LBRACE) {
+			return nil
+		}
+		e.Alternative = p.parseBlockStatement()
+	}
+	return e
+}
+func (p *Parser) parseBlockStatement() *ast.BlockStatement {
+	block := &ast.BlockStatement{Token: p.curToken}
+	block.Statements = []ast.Statement{}
+
+	p.nextToken()
+
+	for !p.curTokenIs(token.RBRACE) && !p.curTokenIs(token.EOF) {
+		s := p.parseStatement()
+		if s != nil {
+			block.Statements = append(block.Statements, s)
+		}
+		p.nextToken()
+	}
+	return block
+}
 func (p *Parser) parseLetStatement() *ast.LetStatement {
 	stmt := &ast.LetStatement{Token: p.curToken}
 
-	if !p.expectPeek(token.IDENT) {
+	if !p.expectNextToken(token.IDENT) {
 		return nil
 	}
 
 	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
 
-	if !p.expectPeek(token.ASSIGN) {
+	if !p.expectNextToken(token.ASSIGN) {
 		return nil
 	}
 
@@ -186,7 +239,7 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
 	return stmt
 }
 
-func (p *Parser) expectPeek(t token.TokenType) bool {
+func (p *Parser) expectNextToken(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
